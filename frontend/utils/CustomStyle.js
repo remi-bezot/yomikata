6a6d58abe45beecf5a8a851a8cd652c9b7223125
Kavita <bkavita1998@gmail.com>
2024-12-09 11:36:05 +0100

    update styles size

--- frontend/utils/CustomStyle.js
+++ frontend/utils/CustomStyle.js
@@ -1,46 +1,54 @@
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse, faBook, faBookmark, faUserGraduate, faUser, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faHouse, faBook, faBookmark, faUserGraduate, faUser, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
-export const Icons = {
-    home: faHouse,
-    lessons: faBook,
-    bookmark: faBookmark,
-    user: faUserGraduate,
-    authuser: faUser, 
-    authpwd: faEyeSlash,
+// export const Icons = {
+//     home: faHouse,
+//     lessons: faBook,
+//     bookmark: faBookmark,
+//     user: faUserGraduate,
+//     authuser: faUser, 
+//     authpwd: faEyeSlash,
 
-};
-
-
-export const colors = {
-    h1: '#CC4646',
-    h2: 'black',
-    h3:'black',
-
-}
+// };
 
 
+// export const colors = {
+//     h1: '#CC4646',
+//     h2: 'black',
+//     h3:'black',
 
-export const button = {
-  backgroundColor: "#EEC1C0",
-  radius: 32,
-};
+// }
 
-export const font = {
-  font: "Noto Sans JP",
-};
 
-export const heading = {
-  font: "Noto Sans JP",
-  color: "#CC4646",
-  iconEffect: "Drop Shadow",
-  borderColor: "#090909",
-  borderPosition: "inside",
-  borderSize: 0.5,
-};
 
-export const navBar = {
-  backgroundColor: "#EEC1C0",
-  borderColor: "#070000",
-};
+// export const button = {
+//   backgroundColor: "#EEC1C0",
+//   radius: 32,
+// };
+
+// export const font = {
+//   font: "Noto Sans JP",
+// };
+
+// export const heading = {
+//   font: "Noto Sans JP",
+//   color: "#CC4646",
+//   iconEffect: "Drop Shadow",
+//   borderColor: "#090909",
+//   borderPosition: "inside",
+//   borderSize: 0.5,
+// };
+
+// export const navBar = {
+//   backgroundColor: "#EEC1C0",
+//   borderColor: "#070000",
+// };
+
+
+export const customStyles = {
+    backgroundColor: "#EEC1C0",
+    borderColor: "#070000",
+    borderRadius:24
+    
+  }
