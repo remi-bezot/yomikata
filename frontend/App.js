c94c84b5800aefa762bd6475ff5bb3793c0fde62
BOUZID Ahmed <ahmahm@hotmail.co.jp>
2024-12-09 11:21:25 +0100

    reducer user, store/provider

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import user from "../reducers/user";
+import { Provider } from "react-redux";
+
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+	reducer: { user },
+});
+
+const reducers = combineReducers({ user });
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<Provider store={store}>
+			<View style={styles.container}>
+				<Text>Open up App.js to start working on your app!</Text>
+				<StatusBar style="auto" />
+			</View>
+		</Provider>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
