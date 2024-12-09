1253c3e9d54037cb804775f01f53ccc128eaeb0c
Rémi Bezot <bezot.remi@gmail.com>
2024-12-09 11:05:01 +0100

    update CustomStyle.js

--- frontend/utils/CustomStyle.js
+++ frontend/utils/CustomStyle.js
@@ -1,7 +1,22 @@
 export const button = {
-  buttons: "#EEC1C0",
+  backgroundColor: "#EEC1C0",
   radius: 32,
 };
+
 export const font = {
-  family: "Noto Sans JP",
+  font: "Noto Sans JP",
+};
+
+export const heading = {
+  font: "Noto Sans JP",
+  color: "#CC4646",
+  iconEffect: "Drop Shadow",
+  borderColor: "#090909",
+  borderPosition: "inside",
+  borderSize: 0.5,
+};
+
+export const navBar = {
+  backgroundColor: "#EEC1C0",
+  borderColor: "#070000",
 };
