59f029f859f718cd4456da0a97c23174e46671fb
Rémi Bezot <bezot.remi@gmail.com>
2024-12-09 11:28:20 +0100

    update CustomStyle.js

--- frontend/utils/CustomStyle.js
+++ frontend/utils/CustomStyle.js
@@ -1,46 +1,31 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faBook,
+  faBookmark,
+  faUserGraduate,
+  faUser,
+  faEyeSlash,
+} from "@fortawesome/free-solid-svg-icons";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse, faBook, faBookmark, faUserGraduate, faUser, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
-
-export const Icons = {
-    home: faHouse,
-    lessons: faBook,
-    bookmark: faBookmark,
-    user: faUserGraduate,
-    authuser: faUser, 
-    authpwd: faEyeSlash,
-
-};
-
-
-export const colors = {
-    h1: '#CC4646',
-    h2: 'black',
-    h3:'black',
-
-}
-
-
-
-export const button = {
-  backgroundColor: "#EEC1C0",
-  radius: 32,
-};
-
-export const font = {
-  font: "Noto Sans JP",
-};
-
-export const heading = {
-  font: "Noto Sans JP",
-  color: "#CC4646",
-  iconEffect: "Drop Shadow",
-  borderColor: "#090909",
-  borderPosition: "inside",
-  borderSize: 0.5,
-};
-
-export const navBar = {
-  backgroundColor: "#EEC1C0",
-  borderColor: "#070000",
+export const customStyles = {
+  iconHome: faHouse,
+  iconLessons: faBook,
+  iconBookmark: faBookmark,
+  inconUser: faUserGraduate,
+  iconAuthuser: faUser,
+  iconAuthpwd: faEyeSlash,
+  headingFont: "Noto Sans JP",
+  headingColor: "#CC4646",
+  headingIconEffect: "Drop Shadow",
+  headingBorderColor: "#090909",
+  headingBorderPosition: "inside",
+  headingBorderSize: 0.5,
+  buttonBackgroundColor: "#EEC1C0",
+  buttonRadius: 12,
+  defaultFontFamily: "Noto Sans JP",
+  navBarBackgroundColor: "#EEC1C0",
+  navBarBorderColor: "#070000",
+  navBarIconSelectedColor: "#D56565",
+  navBarIconSelectedBackgroundColor: "FFFFFF",
 };
