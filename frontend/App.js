4efee9fe53dfecd49baab4b04beef0c4675603ea
Rémi Bezot <bezot.remi@gmail.com>
2024-12-09 11:49:15 +0100

    update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,13 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <TouchableOpacity style={styles.btn}>
-        <Text>Login</Text>
-      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,11 +13,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  btn: {
-    
-  }
+  btn: {},
 });
