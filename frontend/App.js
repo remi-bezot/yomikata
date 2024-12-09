9f7d2c7fbd0af786550ac16b6a37dfd35ca7f8cd
BOUZID Ahmed <ahmahm@hotmail.co.jp>
2024-12-09 15:57:56 +0100

    reducer store logout

--- frontend/App.js
+++ frontend/App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import user from "../reducers/user";
-import { Provider } from "react-redux";
+//imports pour le store
 
+import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
+//configuration du store
 
 const store = configureStore({
 	reducer: { user },
@@ -13,12 +15,14 @@ const reducers = combineReducers({ user });
 
 export default function App() {
 	return (
+		// inscription du Store pour redux
 		<Provider store={store}>
 			<View style={styles.container}>
 				<Text>Open up App.js to start working on your app!</Text>
 				<StatusBar style="auto" />
 			</View>
 		</Provider>
+		// inscription du Store pour redux
 	);
 }
 
