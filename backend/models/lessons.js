d109fd5cf05c532e8c619843e0e4faf85f92a0de
Rémi Bezot <bezot.remi@gmail.com>
2024-12-10 16:55:38 +0100

    update models lessons

--- backend/models/lessons.js
+++ backend/models/lessons.js
@@ -1,29 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const lessonSchema = mongoose.Schema({
-
-  theme : String,
-  level : Number,
-  total_number : Number,
-  
-  dialogue : {
-    romanji : String,
-    japanese : String,
-    english : String,
-    isRead : Boolean,
+  theme: String,
+  level: Number,
+  total_number: Number,
+  dialogue: {
+    speaker: String,
+    romanji: String,
+    japanese: String,
+    english: String,
+    isRead: Boolean,
   },
-  practice : {
-    word_jp : String,
-    good_answer : String,
-    wrong_answer : String,
-    wrong_answer : String,
-    wrong_answer : String,
-    isDone : Boolean, 
+  practice: {
+    word_jp: String,
+    good_answer: String,
+    wrong_answer: String,
+    wrong_answer: String,
+    wrong_answer: String,
+    isDone: Boolean,
   },
- 
-
 });
 
-const Lesson = mongoose.model('lessons', lessonSchema);
+const Lesson = mongoose.model("lessons", lessonSchema);
 
 module.exports = Lesson;
