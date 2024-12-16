4b73b4a7de3d9247733c877cebc9483d65ecc851
Kavita <bkavita1998@gmail.com>
2024-12-16 15:58:37 +0100

    be able to add favorites words from lesson to favorite screen

--- frontend/reducers/favoritesreducer.js
+++ frontend/reducers/favoritesreducer.js
@@ -1,26 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-	value: { words: []},
+  value: { word: [], jlpt: [], grammar: [] },
 };
 
 export const favoriteSlice = createSlice({
-	name: "favorites",
-	initialState,
-	reducers: {
-		getFavorites: (state, action) => {
-			state.value.words = action.payload;
-		},
-		findFavorite: (state, action) => {
-			state.value.words.find(action.payload) 
-		},
-		deleteFavorite: (state, action) => {
-            state.value.words.filter(
-                (e) => e !== action.payload)
-		}
-	},
+  name: "favorites",
+  initialState,
+  reducers: {
+    addFavorite: (state, action) => {
+      state.value.word.push(action.payload.word);
+      state.value.jlpt.push(action.payload.jlpt);
+      state.value.grammar.push(action.payload.grammar);
+    },
+  },
 });
 
-export const { getFavorites, findFavorite, deleteFavorite } = favoriteSlice.actions;
-export default userSlice.reducer;
-
+export const { addFavorite } = favoriteSlice.actions; 
+export default favoriteSlice.reducer; 
