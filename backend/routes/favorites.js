2a0edd15d82e4ee9a03dede912ea6dface6f35df
BOUZID Ahmed <ahmahm@hotmail.co.jp>
2024-12-18 23:36:42 +0100

    verif

--- backend/routes/favorites.js
+++ backend/routes/favorites.js
@@ -8,47 +8,41 @@ const User = require("../models/users");
 router.get("/showFavorites/:token", (req, res) => {
 	const token = req.params.token;
 
-    User.findOne({ token : token }).then((data) => {
-        if (data) {
-            console.log(data);
-            
-            Favorite.find({id_user: data._id})
-                .then((data) => {
-                    res.json({ result: data });
-                });
-        }
-    });
+	User.findOne({ token: token }).then((data) => {
+		if (data) {
+			console.log(data);
 
+			Favorite.find({ id_user: data._id }).then((data) => {
+				res.json({ result: data });
+			});
+		}
+	});
 });
 
 router.post("/createFavorite/:token", (req, res) => {
-
 	User.findOne({ token: req.params.token }).then((data) => {
-console.log('rrr');
+		Favorite.findOne({ id_user: data._id, Word_JP: req.body.wordjp }).then(
+			(element) => {
+				console.log(element);
 
-		Favorite.findOne({id_user: data._id, Word_JP: req.body.wordjp })
-		.then ((element) => {
-			console.log(element);
-			
-			if (element === null) {
-				const newFavorite = new Favorite({
-					Word_JP: req.body.wordjp,
-					Word_EN: req.body.worden,
-					Romanji: req.body.romanji,
-					Grammar: req.body.grammar,
-					isBook: req.body.isbook,
-					id_user: data._id,
-				});
-		
-				newFavorite.save().then((newDoc) => {
-					res.json({ result: true,status:true, data: newDoc });
-					console.log('hshshshsh')
-				})
+				if (element === null) {
+					const newFavorite = new Favorite({
+						Word_JP: req.body.wordjp,
+						Word_EN: req.body.worden,
+						Romanji: req.body.romanji,
+						Grammar: req.body.grammar,
+						isBook: req.body.isbook,
+						id_user: data._id,
+					});
 
-			} })
-		})
-		
-		
+					newFavorite.save().then((newDoc) => {
+						res.json({ result: true, status: true, data: newDoc });
+						console.log("hshshshsh");
+					});
+				}
+			}
+		);
+	});
 });
 
 router.delete("/deleteFavorite/", (req, res) => {
