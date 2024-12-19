7fa00d05d7edf02681febe93f7f6466c4a2588c9
Kavita <bkavita1998@gmail.com>
2024-12-19 15:23:50 +0100

    update word research

--- frontend/components/SignIn.js
+++ frontend/components/SignIn.js
@@ -22,7 +22,7 @@ import { BackendAdress } from "../utils/BackendAdress";
 export default function SignUp() {
   const dispatch = useDispatch();
 
-  const EMAIL_REGEX =
+  const EMAIL_REGEX = 
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   const navigation = useNavigation();
